fdc4c5f4310764d642557bcebc5d6ca9f31a557c
Evanoul <evancantatore4@gmail.com>
2025-11-07 15:53:23 +0000

    feat: Implémentation du toggle dark mode

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ console.log("Script chargé !")
 
 const Titreprincipal = document.querySelector('#Titreprincipal');
 
-Titreprincipal.addEventListener('mouseover',() => {
+Titreprincipal.addEventListener('click', () => {
     Titreprincipal.textContent = "ta cliqué bro";
-    console.log ("Bouton cliqué !")
+    console.log("Bouton cliqué !")
 
 });
 
-  
+const button = document.querySelector('#theme-toggle');
+button.addEventListener('click', () => {
+    console.log("Bouton cliqué !")
+    document.body.classList.toggle('dark-mode');
+    document.header.classList.toggle('dark-mode');
+})
