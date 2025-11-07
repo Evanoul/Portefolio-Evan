392e94ca6a7f7fafa9600ab366f6b85a91841aeb
Evanoul <evancantatore4@gmail.com>
2025-11-07 14:37:21 +0000

    feat: Ajout de script.js et première interaction DOM

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Script chargé !")
+
+const Titreprincipal = document.querySelector('#Titreprincipal');
+
+Titreprincipal.addEventListener('mouseover',() => {
+    Titreprincipal.textContent = "ta cliqué bro";
+    console.log ("Bouton cliqué !")
+
+});
+
+  
